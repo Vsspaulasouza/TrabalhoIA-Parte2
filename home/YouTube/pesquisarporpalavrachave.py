d63a081151d255a60e9efb6e768d2651e794b35f
Vinícius Soares de Paula Souza <69551648+Vsspaulasouza@users.noreply.github.com>
2021-09-19 14:39:15 -0300

    Filtro de linguagem

--- home/YouTube/pesquisarporpalavrachave.py
+++ home/YouTube/pesquisarporpalavrachave.py
@@ -5,7 +5,7 @@ import json
 def pesquisar(palavraChave, order):
     apiKey = "AIzaSyCUVcai7yzkeQrN-yeabuKga1nd4-5Cvpg"
 
-    url = f"https://youtube.googleapis.com/youtube/v3/search?part=id&maxResults=1&order={order}&publishedAfter=2010-01-01T00%3A00%3A00Z&q={palavraChave}&regionCode=BR&type=video&videoEmbeddable=true&key={apiKey}"
+    url = f"https://youtube.googleapis.com/youtube/v3/search?part=id&maxResults=1&order={order}&publishedAfter=2010-01-01T00%3A00%3A00Z&q={palavraChave}&regionCode=BR&relevanceLanguage=pt-BR&type=video&videoEmbeddable=true&key={apiKey}"
 
     videoId = requests.get(url).json()["items"][0]["id"]["videoId"]
     resposta = f'<iframe width="250" height="150" src="https://www.youtube.com/embed/{videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
